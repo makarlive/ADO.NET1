eac12fe42de7c425b39665e724ad75b7618a3095
makarlive <imakarov501@gmail.com>
2018-03-12 11:29:19 +0200

    First Connection

--- [ADO.NET]First/[ADO.NET]First/Program.cs
+++ [ADO.NET]First/[ADO.NET]First/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace _ADO.NET_First
 {
@@ -10,6 +12,15 @@ namespace _ADO.NET_First
     {
         static void Main(string[] args)
         {
+            SqlConnection sql = new SqlConnection(@"Data Source = CR5-13\SQLEXPRESS; Initial Catalog = Publishers; Integrated Security = sspi");
+            sql.Open();
+            SqlCommand sqlcommand = new SqlCommand(@"select * from Authors",sql);
+            SqlDataReader reader = sqlcommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]+" "+reader[1]+" "+reader[2]);
+            }
+            
         }
     }
 }

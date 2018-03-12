d4c0215698bc9ea5558fb2b2061f743e5c13947e
makarlive <imakarov501@gmail.com>
2018-03-12 11:57:36 +0200

    Can execute Query

--- [ADO.NET]First/[ADO.NET]First/Program.cs
+++ [ADO.NET]First/[ADO.NET]First/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.Common;
+using System.Windows.Forms;
 
 namespace _ADO.NET_First
 {
@@ -12,14 +13,37 @@ namespace _ADO.NET_First
     {
         static void Main(string[] args)
         {
-            SqlConnection sql = new SqlConnection(@"Data Source = CR5-13\SQLEXPRESS; Initial Catalog = Publishers; Integrated Security = sspi");
-            sql.Open();
-            SqlCommand sqlcommand = new SqlCommand(@"select * from Authors",sql);
-            SqlDataReader reader = sqlcommand.ExecuteReader();
-            while (reader.Read())
-            {
-                Console.WriteLine(reader[0]+" "+reader[1]+" "+reader[2]);
+            
+                SqlConnection sql = new SqlConnection(@"Data Source = CR5-13\SQLEXPRESS; Initial Catalog = Publishers; Integrated Security = sspi");
+                sql.Open();
+                SqlCommand sqlcommand = new SqlCommand();
+                sqlcommand.Connection = sql;
+                string text;
+                SqlDataReader reader;
+                while (true)
+                {
+                try
+                {
+                    text = Console.ReadLine();
+                    sqlcommand.CommandText = text;
+                    reader = sqlcommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write(reader[i] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    reader.Close();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
             }
+                sql.Close();
             
         }
     }

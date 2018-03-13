0c782d298b04291ef023a4eb1ac173e9199ba4f3
makarlive <imakarov501@gmail.com>
2018-03-13 12:13:32 +0200

    Create Table with c#

--- [ADO.NET]2/[ADO.NET]2/Program.cs
+++ [ADO.NET]2/[ADO.NET]2/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace _ADO.NET_2
 {
+
     class Program
     {
+        static void CreateTable(SqlConnection con,ref SqlCommand cmd, string NameTable, int countParam)
+        {
+            string query = $"create table [{NameTable}]( id int primary key identity, ";
+            for (int i = 0; i < countParam; i++)
+            {
+                Console.Write("Write param name: ");
+                query += Console.ReadLine() + " ";
+                Console.Write("Write param type: ");
+                query += Console.ReadLine() + " not null, ";
+            }
+            query += ");";
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+
+        }
         static void Main(string[] args)
         {
+            using (SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["MCS"].ConnectionString))
+            {
+                //Create Command
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = sqlcon;
+                Console.Write("Write Table Name: ");
+                string tableName = Console.ReadLine();
+                Console.Write("Write quantity param: ");
+                int countParam = Convert.ToInt32(Console.ReadLine());
+                CreateTable(sqlcon, ref cmd, tableName, countParam);
+                
+
+            }
         }
     }
 }

63b4a10493a28155d3c4361ccc66941145ce1c43
makarlive <imakarov501@gmail.com>
2018-03-14 10:33:02 +0200

    Update 14.03.18

---
+++ [ADO.NET]3/[ADO.NET]3/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _ADO.NET_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_table = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cb_table
+            // 
+            this.cb_table.FormattingEnabled = true;
+            this.cb_table.Location = new System.Drawing.Point(12, 12);
+            this.cb_table.Name = "cb_table";
+            this.cb_table.Size = new System.Drawing.Size(633, 21);
+            this.cb_table.TabIndex = 0;
+            this.cb_table.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(633, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 266);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cb_table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_table;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ [ADO.NET]3/[ADO.NET]3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _ADO.NET_3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sql;
+        public Form1()
+        {
+            InitializeComponent();
+            sql = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sql.Open();
+            SqlCommand cmd = new SqlCommand("select * from information_schema.tables", sql);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if(reader[3].ToString() == "BASE TABLE")
+                    cb_table.Items.Add(reader[2]);
+            }
+            sql.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sql.Open();
+            SqlCommand cmd = new SqlCommand($"select * from {cb_table.SelectedItem.ToString()}", sql);
+            SqlDataReader reader = cmd.ExecuteReader();
+            listBox1.Items.Clear();
+            while (reader.Read())
+            {
+                string result="";
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    result += reader[i].ToString() + " ";
+                }
+
+                listBox1.Items.Add(result);
+            }
+            sql.Close();
+        }
+    }
+}
